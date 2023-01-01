dcd5b017309938ea60928211720447595d48924f
jiye Park <parkji3618@naver.com>
2023-01-01 14:31:45 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 35 ms, Memory: 13,604 KB -BaekjoonHub

---
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.cpp
@@ -0,0 +1,151 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// float b, c;
+// double d, e, f;
+// char g;
+// char var[256];
+// long long AB;
+// cin >> a;                            // int 변수 1개 입력받는 예제
+// cin >> b >> c;                       // float 변수 2개 입력받는 예제 
+// cin >> d >> e >> f;                  // double 변수 3개 입력받는 예제
+// cin >> g;                            // char 변수 1개 입력받는 예제
+// cin >> var;                          // 문자열 1개 입력받는 예제
+// cin >> AB;                           // long long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// float b = 1.0, c = 2.0;               
+// double d = 3.0, e = 0.0; f = 1.0;
+// char g = 'b';
+// char var[256] = "ABCDEFG";
+// long long AB = 12345678901234567L;
+// cout << a;                           // int 변수 1개 출력하는 예제
+// cout << b << " " << c;               // float 변수 2개 출력하는 예제
+// cout << d << " " << e << " " << f;   // double 변수 3개 출력하는 예제
+// cout << g;                           // char 변수 1개 출력하는 예제
+// cout << var;                         // 문자열 1개 출력하는 예제
+// cout << AB;                          // long long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+#include<iostream>
+#include<string.h>
+#include<queue>
+
+using namespace std;
+
+int N=100;
+int myMap[100][100];
+bool visited[100][100];
+
+int dx[2] = {0, 0};
+int dy[2] = {1, -1};
+
+struct p
+{
+    int x;
+    int y;
+};
+
+p make_p(int x, int y)
+{
+    p temp;
+    temp.x = x;
+    temp.y = y;
+    return temp;
+}
+
+int bfs(p start)
+{
+    queue<p> q;
+
+    q.push(start);
+    visited[start.x][start.y]=true;
+
+    while(!q.empty())
+    {
+        p cur = q.front();
+        q.pop();
+        
+        //cout<<cur.x<<" "<<cur.y<<endl;
+
+        if(cur.x==99 && myMap[cur.x][cur.y]==2) return 1;
+        if(cur.x==99 && myMap[cur.x][cur.y]!=2) return -1;
+
+        int down=0;
+
+        for(int k=0;k<2;k++)
+        {
+            int next_x = cur.x + dx[k];
+            int next_y = cur.y + dy[k];
+
+
+            if(next_x>=0 && next_x<100 && next_y>=0 && next_y<100 && !visited[next_x][next_y] && myMap[next_x][next_y]!=0)
+            {
+                //cout<<"turn"<<endl;
+                q.push(make_p(next_x, next_y));
+                visited[next_x][next_y]=true;
+                break;
+            }
+            else
+            {
+                down+=1;
+            }
+        }
+
+        if(down==2)
+        {
+            q.push(make_p(cur.x+1, cur.y));
+            visited[cur.x+1][cur.y]=true;
+        }
+    }
+}
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+	/*
+	   아래의 freopen 함수는 input.txt 를 read only 형식으로 연 후,
+	   앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
+	   //여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
+	   freopen 함수를 이용하면 이후 cin 을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+	   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 함수를 사용하셔도 좋습니다.
+	   freopen 함수를 사용하기 위해서는 #include <cstdio>, 혹은 #include <stdio.h> 가 필요합니다.
+	   단, 채점을 위해 코드를 제출하실 때에는 반드시 freopen 함수를 지우거나 주석 처리 하셔야 합니다.
+	*/
+	//freopen("input.txt", "r", stdin);
+	/*
+	   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+	*/
+	for(test_case = 1; test_case <= 10; ++test_case)
+	{
+		cin>>T;
+		memset(myMap, 0, sizeof(myMap));
+
+        for(int j=0;j<N;j++)
+        {
+            for(int k=0;k<N;k++)
+            {
+                cin>>myMap[j][k];
+            }
+        }
+
+        for(int j=0;j<N;j++)
+        {
+            if(myMap[0][j]==1)
+            {
+                memset(visited, 0, sizeof(visited));
+                int answer = bfs(make_p(0, j));
+
+                if(answer==-1) continue;
+                if(answer==1)
+                {
+                    cout<<"#"<<test_case<<" "<<j<<endl;
+                    break;
+                }
+            }
+        }
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
