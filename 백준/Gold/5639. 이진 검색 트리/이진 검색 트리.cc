261155dab5941ad0d92f7de8f87763dbaee77d76
jiye Park <parkji3618@naver.com>
2023-03-19 16:27:55 +0900

    [Gold V] Title: 이진 검색 트리, Time: 568 ms, Memory: 2472 KB -BaekjoonHub

---
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#define FASTIO ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int data) : data(data), left(nullptr), right(nullptr){};
+};
+
+void make_tree(Node *node, int x)
+{
+    if (x < node->data)
+    {
+        if (node->left == nullptr)
+        {
+            node->left = new Node(x);
+        }
+        else
+        {
+            make_tree(node->left, x);
+        }
+    }
+    else
+    {
+        if (node->right == nullptr)
+        {
+            node->right = new Node(x);
+        }
+        else
+        {
+            make_tree(node->right, x);
+        }
+    }
+}
+
+void print_tree(Node *node)
+{
+    if (node->left != nullptr)
+        print_tree(node->left);
+    if (node->right != nullptr)
+        print_tree(node->right);
+    cout << node->data << endl;
+}
+
+int main()
+{
+    int x;
+    cin >> x;
+
+    Node *root = new Node(x);
+
+    while (cin >> x)
+    {
+        make_tree(root, x);
+    }
+
+    print_tree(root);
+}
