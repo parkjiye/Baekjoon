32fcd60d645c55b683439e006ceb80dbfcc6c73e
jiye Park <parkji3618@naver.com>
2023-04-21 20:04:57 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    
+    int answer = 0;
+    
+    for(int i=1;i<=n;i++)
+    {
+        answer+=i;
+    }
+    
+    cout<<answer<<endl;
+    return 0;
+}
