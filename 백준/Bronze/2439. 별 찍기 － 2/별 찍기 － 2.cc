97d9efa0e733ddb251b86db01c5db891f492606e
jiye Park <parkji3618@naver.com>
2023-04-30 21:19:40 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = 1; j <= N - i; j++)
+        {
+            cout << " ";
+        }
+
+        for (int k = N - i; k < N; k++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
