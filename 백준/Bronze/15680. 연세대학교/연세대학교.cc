d33913d9ae3887cf7371b7bc0af26cabdb860ecb
jiye Park <parkji3618@naver.com>
2023-06-05 22:03:56 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin>>N;
+    
+    if(N)
+    {
+        cout<<"Leading the Way to the Future"<<endl;
+    }
+    else
+    {
+        cout<<"YONSEI"<<endl;
+    }
+    
+    return 0;
+}
