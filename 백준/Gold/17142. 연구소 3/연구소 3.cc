17d54bab7766c44e25707f7432ff3740d40ea0bd
jiye Park <parkji3618@naver.com>
2023-06-26 22:55:17 +0900

    [Gold III] Title: 연구소 3, Time: 16 ms, Memory: 2052 KB -BaekjoonHub

--- 백준/Gold/17142. 연구소 3/연구소 3.cc
+++ 백준/Gold/17142. 연구소 3/연구소 3.cc
@@ -1,184 +1,156 @@
-#include<iostream>
-#include<queue>
-#include<vector>
-#include<string.h>
+#include <iostream>
+#include <queue>
+#include <string.h>
 
-int myMap[51][51];
-int Table[51][51];
-bool check[51][51];
-
-int arr[51*51];
-
-struct p {
-    int x;
-    int y;
-};
-
-p make_p(int y, int x) {
-    p temp;
-
-    temp.x = x;
-    temp.y = y;
-
-    return temp;
-}
-
-int dy[4] = {-1, 0, 0, 1};
-int dx[4] = {0, -1, 1, 0};
-
-p diff[11];
-int diff_len=0;
+#define FASTIO() ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
 
 using namespace std;
 
-vector<int> list;
+int N, M;
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0, 0, -1, 1};
 
-int n, m;
-
-void BFS(p start)
+struct virus
 {
-    queue<p> Queue;
-    
-    memset(check, 0, sizeof(check));
+    int x;
+    int y;
 
-    Queue.push(make_p(start.y, start.x));
-    check[start.y][start.x] = true;
-    Table[start.y][start.x] = 0;
+    virus(int x, int y) : x(x), y(y) {}
+    virus() {}
+};
 
-    while(!Queue.empty()) {
-        p current = Queue.front();
-        Queue.pop();
+struct p
+{
+    int x;
+    int y;
 
-        for(int k=0;k<4;k++) {
-            int next_y = current.y+dy[k];
-            int next_x = current.x+dx[k];
+    p(int x, int y) : x(x), y(y) {}
+    p() {}
+};
 
-            if(0<=next_y && next_y<n && 0<=next_x && next_x<n && !check[next_y][next_x] && myMap[next_y][next_x]!=1 && Table[next_y][next_x] > Table[current.y][current.x]+1)
-            {
-                Queue.push(make_p(next_y, next_x));
-                check[next_y][next_x] = true;
-                Table[next_y][next_x] = Table[current.y][current.x] + 1;
-            }
+int myMap[55][55];
+bool visited[55][55];
+int dist[55][55];
+vector<virus> v;
+int path[55];
+vector<virus> active;
+
+int answer;
+
+// 바이러스 선택
+void bfs(int idx)
+{
+    if (idx == M)
+    {
+        queue<p> q;
+        memset(visited, 0, sizeof(visited));
+        memset(dist, 0, sizeof(dist));
+        int local_max = 0;
+
+        for (int i = 0; i < M; i++)
+        {
+            active[i] = v[path[i]];
+            virus cur = active[i];
+            q.push(p(cur.x, cur.y));
+            visited[cur.x][cur.y] = true;
         }
-    }
 
-}
+        while (!q.empty())
+        {
+            p cur = q.front();
+            q.pop();
 
-int pathlist()
-{
-    int result = 0;
-
-    for(int i=0; i<n;i++) {
-        for(int j=0; j<n;j++) {
-            if(Table[i][j]==987987987)
+            for (int k = 0; k < 4; k++)
             {
-                if(myMap[i][j]!=1)
+                int next_x = cur.x + dx[k];
+                int next_y = cur.y + dy[k];
+
+                if (next_x >= 0 && next_y >= 0 && next_x < N && next_y < N && !visited[next_x][next_y] && myMap[next_x][next_y] != 1)
                 {
-                        list.push_back(-1);
-                        return 0;
+                    visited[next_x][next_y] = true;
+                    q.push(p(next_x, next_y));
+                    dist[next_x][next_y] = dist[cur.x][cur.y] + 1;
                 }
             }
-            else
+        }
+
+        bool allspread = true;
+
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
             {
-                if(myMap[i][j]!=2)
+                if (myMap[i][j] != 1 && !visited[i][j] && myMap[i][j] != 2)
                 {
-                    result = max(result, Table[i][j]);
+                    allspread = false;
+                    break;
                 }
             }
         }
-    }
-   
 
-    list.push_back(result);
-    return 0;
-}
-
-
-void getResult(int inx)
-{
-    //지금 inx번째의 for문이다
-    //inx번째 for문의 변수 값을 지정해줘야함
-
-    if(inx >= m)
-    {
-        for(int i=0;i<n;i++)
+        if (allspread)
         {
-            for(int j=0;j<n;j++)
+            for (int i = 0; i < N; i++)
             {
-                Table[i][j]=987987987;
+                for (int j = 0; j < N; j++)
+                {
+                    if (local_max < dist[i][j] && myMap[i][j] != 2)
+                    {
+                        local_max = dist[i][j];
+                    }
+                }
             }
-        }
 
-        p test_diff[m];
-        for(int i=0;i<m;i++)
-        {
-            test_diff[i].y=diff[arr[i]].y;
-            test_diff[i].x=diff[arr[i]].x;
-        }
-
-        for(int i=0;i<m;i++)
-        {
-            //cout<<test_diff[i].x<<test_diff[i].y<<endl;
-            BFS(test_diff[i]);
-        }
-
-        /*for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
+            if (answer > local_max)
             {
-                if(Table[i][j]==987987987) cout<<"-";
-                else cout<<Table[i][j];
+                answer = local_max;
             }
-            cout<<endl;
-        }*/
-
-        pathlist();
-
+        }
     }
-    else
+    for (int i = 0; i < v.size(); i++)
     {
-        for(int i=0;i<diff_len;i++)
+        if (idx == 0 || path[idx - 1] < i)
         {
-            if(inx==0 || arr[inx-1]<i){
-                arr[inx]=i;
-                getResult(inx+1);
-            }
+            path[idx] = i;
+            bfs(idx + 1);
         }
     }
 }
-
-//getResult(0) -> arr[0]=0 -> getResult(1) -> arr[1]=1 ->getResult(2) -> arr[2]=2
-//arr[0]=0 arr[1]=1 
 
 int main()
 {
-    cin>>n>>m;
+    FASTIO();
+    cin >> N >> M;
 
-    for(int i=0;i<n;i++)
+    memset(myMap, 0, sizeof(myMap));
+    memset(visited, 0, sizeof(visited));
+    active.resize(M);
+
+    answer = 30000;
+
+    for (int i = 0; i < N; i++)
     {
-        for(int j=0;j<n;j++)
+        for (int j = 0; j < N; j++)
         {
-            cin>>myMap[i][j];
+            int x;
+            cin >> x;
 
-            if(myMap[i][j]==2)
+            myMap[i][j] = x;
+
+            if (x == 2)
             {
-                diff[diff_len].y=i;
-                diff[diff_len].x=j;
-
-                diff_len++;
+                v.push_back(virus(i, j));
             }
         }
     }
-    getResult(0);
 
-    int answer=987987987;
+    bfs(0);
 
-    for(int i=0;i<list.size();i++)
+    if (answer == 30000)
     {
-        //cout<<list[i]<<" ";
-        if(list[i]!=-1 && answer>list[i]) answer=list[i];
+        cout << -1 << endl;
+        return 0;
     }
 
-    if(answer==987987987) cout<<"-1"<<endl;
-    else cout<<answer<<endl;
+    cout << answer << endl;
 }
