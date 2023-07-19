08b56ae58b9f1b5e92a32e0dd3b6aecdca250f5a
jiye Park <parkji3618@naver.com>
2023-07-19 20:42:10 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string sen;
+    cin>>sen;
+    
+    if(sen=="SONGDO")
+    {
+        cout<<"HIGHSCHOOL"<<endl;
+    }
+    else if(sen=="CODE")
+    {
+        cout<<"MASTER"<<endl;
+    }
+    else if(sen=="2023")
+    {
+        cout<<"0611"<<endl;
+    }
+    else
+    {
+        cout<<"CONTEST"<<endl;
+    }
+}
