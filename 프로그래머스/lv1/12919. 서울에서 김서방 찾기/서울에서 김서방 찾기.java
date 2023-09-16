8f86d45054513d9db16e97b341de055384fa0647
jiye Park <parkji3618@naver.com>
2023-09-17 00:09:08 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 5.95 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,14 +1,13 @@
+import java.util.*;
+
 class Solution {
     public String solution(String[] seoul) {
         String answer = "";
         
-        for(int i=1;i<=seoul.length;i++)
-        {
-            if(seoul[i-1].equals("Kim"))
-            {
-                answer="김서방은 "+(i-1)+"에 있다";
-            }
-        }
+        List<String> strlist = new ArrayList<>(Arrays.asList(seoul));
+        int idx = strlist.indexOf("Kim");
+        
+        answer = "김서방은 "+(idx)+"에 있다";
         return answer;
     }
 }
