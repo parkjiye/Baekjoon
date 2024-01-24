bed619e1f55933789f591dd0cbf3552c124ccbc5
jiye Park <parkji3618@naver.com>
2024-01-24 22:54:32 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char c;
+    cin>>c;
+    
+    if(c=='N' || c=='n')
+    {
+        cout<<"Naver D2"<<endl;
+    }
+    else
+    {
+        cout<<"Naver Whale"<<endl;
+    }
+}
