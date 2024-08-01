648cd21ed2a3cd0e9d5707eaa08e7c6aa1a90275
jiye Park <parkji3618@naver.com>
2024-08-01 20:46:57 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#define FASTIO() ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+
+using namespace std;
+
+void chatbot(int i, int N)
+{
+
+    for (int t = 0; t < i; t++)
+    {
+        cout << "____";
+    }
+    cout << "\"재귀함수가 뭔가요?\"" << "\n";
+    if (i == N)
+    {
+        for (int t = 0; t < i; t++)
+        {
+            cout << "____";
+        }
+        cout << "\"재귀함수는 자기 자신을 호출하는 함수라네\"" << "\n";
+        for (int t = 0; t < i; t++)
+        {
+            cout << "____";
+        }
+        cout << "라고 답변하였지." << "\n";
+        return;
+    }
+
+    for (int t = 0; t < i; t++)
+    {
+        cout << "____";
+    }
+    cout << "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어." << "\n";
+    for (int t = 0; t < i; t++)
+    {
+        cout << "____";
+    }
+    cout << "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지." << "\n";
+    for (int t = 0; t < i; t++)
+    {
+        cout << "____";
+    }
+    cout << "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"" << "\n";
+
+    chatbot(i + 1, N);
+
+    for (int t = 0; t < i; t++)
+    {
+        cout << "____";
+    }
+    cout << "라고 답변하였지." << "\n";
+}
+
+int main()
+{
+    FASTIO();
+    int N;
+    cin >> N;
+
+    cout << "어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다." << "\n";
+
+    chatbot(0, N);
+}
