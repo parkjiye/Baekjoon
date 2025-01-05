dd0fb5968d805b6d26bde24ec2eed608ce714239
jiye Park <parkji3618@naver.com>
2025-01-05 20:08:44 +0900

    [level 2] Title: 교점에 별 만들기, Time: 35.83 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
+++ 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
@@ -1,106 +1,81 @@
-import java.util.List;
-import java.util.ArrayList;
 import java.util.*;
 
 class Solution {
+    public int width;
+    public int height;
+    public long mx = Long.MIN_VALUE;
+    public long my = Long.MIN_VALUE;
+    public long nx = Long.MAX_VALUE;
+    public long ny = Long.MAX_VALUE;
     
-    public static class Point {
-        private final long x, y;
+    class star {
+        public long x, y;
         
-        private Point(long x, long y) {
+        public star(long x, long y){
             this.x = x;
             this.y = y;
         }
+    
+    
     }
     
-    public Point intersection(long a1, long b1, long c1, long a2, long b2, long c2) {
-        double x = (double)(b1*c2 - c1*b2) / (a1*b2-b1*a2);
-        double y = (double) (c1*a2-a1*c2) / (a1*b2-b1*a2);
-        
-        if(x%1!=0 || y%1!=0)
-        {
-            return null;
+    public star makestar(double x1, double x2, double y1, double y2, double z1, double z2){
+            double x = (double) (((y1*z2) - (z1*y2)) / ((x1*y2)-(y1*x2)));
+            double y = (double) (((z1*x2) - (x1*z2)) / ((x1*y2)-(y1*x2)));
+            
+            if(x % 1 == 0.0 && y % 1 == 0.0){
+                star s = new star((long)x, (long)y);
+                return s;
+            }
+            else {
+                return null;
+            }
         }
-        
-        
-        return new Point((long)x, (long)y);
-    }
     
-    public Point getMaxPoint(List<Point> points) {
-        long x = Long.MIN_VALUE;
-        long y = Long.MIN_VALUE;
-        
-        for(Point p:points)
-        {
-            if(p.x>x) x = p.x;
-            if(p.y>y) y = p.y;
+    public void calculate(ArrayList<star> stars){
+       
+        for(star s: stars){
+            if(s.x>mx) mx = s.x;
+            if(s.y>my) my = s.y;
         }
-        
-        return new Point(x,y);
-        
-    }
-    
-    public Point getMinPoint(List<Point> points) {
-        long x = Long.MAX_VALUE;
-        long y = Long.MAX_VALUE;
-        
-        for(Point p : points)
-        {
-            if(p.x<x)x=p.x;
-            if(p.y<y)y=p.y;
+            
+        for(star s: stars){
+            if(s.x<nx) nx = s.x;
+            if(s.y<ny) ny = s.y;
         }
-        
-        return new Point(x,y);
+            
+        width = (int) (mx - nx)+1;
+        height = (int) (my - ny)+1;
     }
     
     public String[] solution(int[][] line) {
-        String[] answer = {};
+        ArrayList<star> stars = new ArrayList<>();
         
-        List<Point> points = new ArrayList<>();
-        
-        for(int i=0;i<line.length;i++)
-        {
-            for(int j=i+1;j<line.length;j++)
-            {
-                Point temp = intersection(line[i][0], line[i][1], line[i][2], line[j][0], line[j][1], line[j][2]);
+        for(int i=0;i<line.length;i++){
+            for(int j=i+1;j<line.length;j++){
+                if(line[i][0]*line[j][1]==line[i][1]*line[j][0]) continue;
+                star s = makestar(line[i][0], line[j][0], line[i][1], line[j][1], line[i][2], line[j][2]);
                 
-                if(temp!=null)
-                {
-                    points.add(temp);
-                }
+                if(s==null) continue;
+                stars.add(s);
+
             }
         }
         
-        Point maxPoint = getMaxPoint(points);
-        Point minPoint = getMinPoint(points);
-        
-        long width = maxPoint.x-minPoint.x+1;
-        long height = maxPoint.y-minPoint.y+1;
-        
-        answer=new String[(int)height];
-        
-        for(int i=0;i<height;i++)
-        {
-            String sen="";
-            for(int j=0;j<width;j++)
-            {
-                sen+='.';
-            }
-            answer[i]=sen;
+        calculate(stars);
+    
+        char[][] sky = new char[height][width];
+        for(char[] row:sky) {
+            Arrays.fill(row, '.');
         }
         
-        for(Point p:points)
-        {
-            //System.out.println(p.x+" "+p.y);
-                
-            if(p.x>=minPoint.x && p.y>=minPoint.y && p.x<=maxPoint.x && p.y<=maxPoint.y)
-            {
-                Point cur = new Point(maxPoint.y-p.y, p.x-minPoint.x);          
-                
-                //System.out.println(cur.x + " "+cur.y);
-                String temp = answer[(int)cur.x];
-                answer[(int)cur.x]= temp.substring(0, (int)(cur.y))+'*'+temp.substring((int)(cur.y)+1);
-            }
+        for(star s: stars){
+            sky[(int)(my-s.y)][(int)(s.x-nx)]='*';
+        }
+        
+        String[] answer = new String[sky.length];
+        for(int i=0;i<answer.length;i++){
+            answer[i]=new String(sky[i]);
         }
         
         
