77ef922c3b5dfd2e86947f5b5cd73778356a3a28
jiye Park <parkji3618@naver.com>
2025-01-07 00:07:23 +0900

    [level 2] Title: 삼각 달팽이, Time: 19.39 ms, Memory: 145 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,55 +1,76 @@
 class Solution {
+    public int x = 0;
+    public int y = 0;
+    public int[][] arr;
+    public int num=1;
+    
+    public void moveRight(int n){
+        for(int i=0;i<n;i++){
+            arr[x][y]=num;
+            num+=1;
+            y+=1;
+        }
+        y-=1;
+    }
+    
+    public void moveDown(int n){
+        for(int i=0;i<n;i++){
+            arr[x][y]=num;
+            num+=1;
+            x+=1;
+        }
+        x-=1;
+    }
+    
+    public void moveDiag(int n){
+        for(int i=0;i<n;i++){
+            arr[x][y]=num;
+            num+=1;
+            x-=1;
+            y-=1;
+        }
+        x+=1;
+        y+=1;
+    }
+    
     public int[] solution(int n) {
+        arr = new int[n][n];
+        int cnt = n;
         
-        int [][] arr = new int[n][n];
-        
-        int[] dx={1, 0, -1};
-        int[] dy={0, 1, -1};
-        
-        int total;
-        
-        if(n%2==0)
-        {
-            total = (1+n)*(n/2);
+        while(cnt>0){
+            moveDown(cnt);
+            y+=1;
+            cnt-=1;
+            if(cnt<=0) break;
+            
+            moveRight(cnt);
+            x-=1;y-=1;
+            cnt-=1;
+            if(cnt<=0) break;
+            
+            moveDiag(cnt);
+            x+=1;
+            cnt-=1;
+            if(cnt<=0) break;
         }
-        else
-        {
-            total = (1+n)*(n/2) + (n/2)+1;
-        }
         
-        int[] answer = new int[total];
-        
-        int cnt=1;
-        int i=-1;
-        int j=0;
-        int d=0;
-        int cycle=n;
-        
-        while(cnt<=total)
-        {
-            for(int k=0;k<cycle;k++)
-            {
-                
-                i+=dx[d%3];
-                j+=dy[d%3];
-                arr[i][j]=cnt;
-                //System.out.println(cnt+" "+i+" "+j+" "+k);
-                cnt+=1;
+        /*for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(arr[i][j]+" ");
             }
-            d+=1;
-            cycle--;
-        }
-        int idx=0;
+            System.out.println();
+        }*/
         
-        for(int t=0;t<n;t++)
-        {
-            for(int l=0;l<=t;l++)
-            {
-                //System.out.println(arr[t][l]);
-                answer[idx]=arr[t][l];
-                idx+=1;
+        int[] answer = new int[num-1];
+        
+        int tmp=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                answer[tmp]=arr[i][j];
+                tmp+=1;
             }
         }
+        
         return answer;
     }
 }
