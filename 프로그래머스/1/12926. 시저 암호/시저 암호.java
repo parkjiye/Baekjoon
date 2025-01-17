2267e274d3c92073fc15540b809722af7cdd276f
jiye Park <parkji3618@naver.com>
2025-01-17 20:42:17 +0900

    [level 1] Title: 시저 암호, Time: 0.82 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,32 +1,28 @@
 class Solution {
     public String solution(String s, int n) {
-        String answer = "";
-        
+        char[] arr = s.toCharArray();
         StringBuilder sb = new StringBuilder();
-        for(int i=0;i<s.length();i++) {
+        for(int i=0;i<s.length();i++){
+            if(arr[i]==' ') {
+                sb = sb.append(arr[i]);
+            }
+            else {
+                int num = (int)arr[i]+n;
+                if(arr[i]<91 && num>=91) {
+                    sb = sb.append((char)(num%91+65));
+                }
+                else if(arr[i]<123 && num>=123) {
+                    sb = sb.append((char)(num%123+97));
+                }
+                else {
+                    sb = sb.append((char)num);
+                }
+                
+            }
+            //System.out.println((int)arr[i] + n);
             
-            char c = s.charAt(i);
-            
-            if(c==' ')
-            {
-                sb.append(c);
-            }
-            else if(c<='Z' && c+n>'Z')
-            {
-                sb.append((char)(c+n-26));
-            }
-            else if(c>='a' && c+n>'z')
-            {
-                sb.append((char)(c+n-26));
-            }
-            else
-            {
-                sb.append((char)(c+n));
-            }
         }
-        
-        answer = sb.toString();
-        
+        String answer = sb.toString();
         return answer;
     }
 }
