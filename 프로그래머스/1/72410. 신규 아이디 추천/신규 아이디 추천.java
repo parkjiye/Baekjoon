68d7e50791e83d0b8ee1b10b882acdd8fbe11dbf
jiye Park <parkji3618@naver.com>
2025-01-25 21:20:00 +0900

    [level 1] Title: 신규 아이디 추천, Time: 16.79 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,26 +1,29 @@
 class Solution {
     public String solution(String new_id) {
         
-        new_id=new_id.toLowerCase();
+        //1단계 소문자 치환
+        new_id = new_id.toLowerCase();
         
-        new_id=new_id.replaceAll("[^a-z0-9\\-_.]", "");
-        new_id = new_id.replaceAll("\\.+",".");
-        new_id=new_id.replaceAll("^\\.+|\\.+$","");
+        //2단계 특정 문자 제거
+        new_id = new_id.replaceAll("[^a-z0-9\\-_.]", "");
         
-        if(new_id.length()==0)
-        {
-            new_id="a";
+        //3단계 마침표가 2번 이상 연속된 부분을 하나의 마침표로 치환
+        new_id = new_id.replaceAll("\\.+", ".");
+        
+        //4단계 마침표가 처음이나 끝에 위치한다면 제거
+        new_id = new_id.replaceAll("^\\.+|\\.+$", "");
+        
+        //5단계
+        if(new_id.isEmpty()) new_id="a";
+        
+        //6단계
+        if(new_id.length()>=16) {
+            new_id = new_id.substring(0, 15);
+            new_id = new_id.replaceAll("\\.+$", "");
         }
         
-        if(new_id.length()>=16)
-        {
-            new_id = new_id.substring(0,15);
-            
-            new_id = new_id.replaceAll("\\.+$","");
-        }
-        
-        while(new_id.length()<3)
-        {
+        //7단계
+        while(new_id.length()<3){
             new_id+=new_id.charAt(new_id.length()-1);
         }
         
