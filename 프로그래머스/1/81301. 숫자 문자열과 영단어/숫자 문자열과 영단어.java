4c1220e31d1de9e8a638886111775b7fc6325f37
jiye Park <parkji3618@naver.com>
2025-01-25 20:55:30 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.08 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,79 +1,12 @@
 class Solution {
-    
-    public char changetoNum(String tmp)
-    {
-        if(tmp.equals("zero"))
-        {
-            return '0';
-        }
-        else if(tmp.equals("one"))
-        {
-            return '1';
-        }
-        else if(tmp.equals("two"))
-        {
-            return '2';
-        }
-        else if(tmp.equals("three"))
-        {
-            return '3';
-        }
-        else if(tmp.equals("four"))
-        {
-            return '4';
-        }
-        else if(tmp.equals("five"))
-        {
-            return '5';
-        }
-        else if(tmp.equals("six"))
-        {
-            return '6';
-        }
-        else if(tmp.equals("seven"))
-        {
-            return '7';
-        }
-        else if(tmp.equals("eight"))
-        {
-            return '8';
-        }
-        else if(tmp.equals("nine"))
-        {
-            return '9';
-        }
-        else
-        {
-            return '*';
-        }
-    }
+    private String num[] = {"zero", "one","two", "three", "four", "five", "six", "seven", "eight", "nine" };
     
     public int solution(String s) {
-        int answer = 0;
         
-        StringBuilder sb = new StringBuilder();
-        StringBuilder ans = new StringBuilder();
-        
-        for(char c:s.toCharArray())
-        {
-            if(c>='A' && c<='z')
-            {
-                sb.append(c);
-                
-                char tmp = changetoNum(sb.toString());
-                
-                if(tmp!='*')
-                {
-                    ans.append(tmp);
-                    sb = new StringBuilder();
-                }
-            }
-            else
-            {
-                ans.append(c);
-            }
+        for(int i=0;i<10;i++){
+            s = s.replace(num[i], Integer.toString(i));
         }
-                
-        return Integer.parseInt(ans.toString());
+        
+        return Integer.parseInt(s);
     }
 }
