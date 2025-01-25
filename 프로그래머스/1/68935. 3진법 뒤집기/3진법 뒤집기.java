64f212d531bc41ecc65ba5dbb98c7cf32c8f7982
jiye Park <parkji3618@naver.com>
2025-01-25 20:44:55 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.07 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,17 +1,8 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        
-        String s = Integer.toString(n,3);
         StringBuilder sb = new StringBuilder();
-        
-        for(char c: s.toCharArray()) {
-            sb.append(c);
-        }
-        
-        sb = sb.reverse();
-        s = sb.toString();
-        
-        return Integer.valueOf(s, 3);
+        sb.append(Integer.toString(n, 3));
+        sb.reverse();
+        return Integer.parseInt(sb.toString(), 3);
     }
 }
