fa9f1fa1d7fc8dde89fd550dc452a0e3fa57382b
jiye Park <parkji3618@naver.com>
2025-01-25 21:08:24 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.16 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,8 +1,6 @@
 class Solution {
     public boolean solution(String s) {
         
-        boolean answer = (s.matches("[0-9]*") && (s.length()==4 || s.length()==6));
-        
-        return answer;
+        return s.matches("[0-9]{4}||[0-9]{6}");
     }
 }
