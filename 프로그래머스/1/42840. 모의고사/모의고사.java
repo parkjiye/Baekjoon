f666fe67c9928d74b44f200ba21bfd8b02748ece
jiye Park <parkji3618@naver.com>
2025-02-07 21:33:46 +0900

    [level 1] Title: 모의고사, Time: 1.69 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,34 +1,31 @@
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 
 class Solution {
-    private static final int[][] prob = {{1,2,3,4,5},{2,1,2,3,2,4,2,5},{3,3,1,1,2,2,4,4,5,5}};
+    private int[][] student = {{1, 2, 3, 4, 5}, {2, 1, 2, 3, 2, 4, 2, 5}, {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
     
     public int[] solution(int[] answers) {
-        int[] answer = {0, 0, 0};
-        
-        for(int i=0;i<3;i++) {
-            for(int j=0;j<answers.length;j++) {
-                if(answers[j]==prob[i][j%prob[i].length]) {
-                    answer[i]+=1;
+        int[] score = {0, 0, 0};
+        for(int i=0;i<answers.length;i++) {
+            for(int j=0;j<3;j++) {
+                if(answers[i]==student[j][i%student[j].length]) {
+                    score[j]++;
                 }
             }
         }
+        int max = Math.max(Math.max(score[0], score[1]), score[2]);
+        List<Integer> list = new ArrayList<>();
         
-        int max_value=0;
+        for(int i=0;i<3;i++){
+            if(max==score[i]) list.add(i+1);
+        }
         
-        for(int i=0;i<3;i++) {
-            if(answer[i]>max_value) {
-                max_value=answer[i];
-            }
+        int[] answer = new int[list.size()];
+        int i=0;
+        
+        for(int l:list){
+            answer[i++]=l;
         }
-        List<Integer> ans = new ArrayList<>();
-
-        for(int i=0;i<3;i++) {
-            if(answer[i]==max_value) {
-                ans.add(i+1);
-            }
-        }
-        return ans.stream().mapToInt(Integer::intValue).toArray();
+        
+        return answer;
     }
 }
