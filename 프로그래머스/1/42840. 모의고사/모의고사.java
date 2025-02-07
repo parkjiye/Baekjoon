74272615841761614f2a12ca77310a0d02fd50d9
jiye Park <parkji3618@naver.com>
2025-02-07 21:41:08 +0900

    [level 1] Title: 모의고사, Time: 10.68 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     private int[][] student = {{1, 2, 3, 4, 5}, {2, 1, 2, 3, 2, 4, 2, 5}, {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
@@ -12,20 +13,8 @@ class Solution {
                 }
             }
         }
-        int max = Math.max(Math.max(score[0], score[1]), score[2]);
-        List<Integer> list = new ArrayList<>();
+        final int max = Math.max(Math.max(score[0], score[1]), score[2]);
         
-        for(int i=0;i<3;i++){
-            if(max==score[i]) list.add(i+1);
-        }
-        
-        int[] answer = new int[list.size()];
-        int i=0;
-        
-        for(int l:list){
-            answer[i++]=l;
-        }
-        
-        return answer;
+        return IntStream.range(0, 3).filter(i->score[i]==max).map(i->i+1).toArray();
     }
 }
